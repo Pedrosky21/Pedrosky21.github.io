01fbc7e6ab8a577d98a01ca032f6e21f6b8fbcb6
Pedro Arreguez <pedroarreguez21@gmail.com>
2024-07-24 04:41:43 -0300

    carousel a about me y mejoras

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Movies from './components/Movies';
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Necesaria a importar para el carousel de bootstrap
 
 const App = () => {
     return (
